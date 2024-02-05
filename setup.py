3327f5bcff3605827ec20e94d9d7f4120b5098a9
= <=>
2024-02-05 11:58:02 -0300

    Refatoração dos caminhos

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from setuptools import setup, find_packages
-
 setup(
     name='PyJPDF',
     version='0.1.3',
     author='Jasson Carvalho',
     description='A simple library to extract text from PDF files.',
     url='https://github.com/LordWaif/PyJPDF',
-    packages=find_packages(),
+    packages=['PyJPDF'],
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     classifiers=[
@@ -21,7 +20,6 @@ setup(
         'Programming Language :: Python :: 3.10',
     ],
     python_requires='>=3.6',
-    py_modules=['pyjpdf','utils'],
     install_requires=[
         'selenium',
         'pyperclip',
